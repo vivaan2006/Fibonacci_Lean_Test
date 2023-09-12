59cae5f1b0b8b08b8eac6dff5b2ff0a735abbc48
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-09-12 08:03:12 -0600

    Update Commmutative.lean

--- Commmutative.lean
+++ Commmutative.lean
@@ -1,49 +1,38 @@
--- 25:6:error: rewrite tactic failed, lemma is not an equality nor a iff
--- state:
--- n : my_nat,
--- ih' : my_add my_nat.zero n = my_add n my_nat.zero
--- ⊢ my_nat.succ (my_add n my_nat.zero) = my_nat.succ n
-
-inductive my_nat
+inductive my_nat where
 | zero : my_nat
 | succ : my_nat → my_nat
 
-def my_add : my_nat → my_nat → my_nat
-| m my_nat.zero     := m
-| m (my_nat.succ n) := my_nat.succ (my_add m n)
+-- Slight rewrite of the my_add function to make it work with Lean4 syntax
 
-theorem my_add_comm : ∀ (m n : my_nat), my_add m n = my_add n m :=
+def my_add (m : my_nat) (n : my_nat) : my_nat :=
+  match n with
+  | (my_nat.zero) => m
+  | (my_nat.succ n') => my_nat.succ (my_add m n')
 
-begin
-  intros m n,
-  induction m with m ih,
-  { 
-    induction n with n ih',
-    
-    {
-      refl,
-    },
-    
-    { 
-      simp [my_add],
-      rw [ih', my_nat.succ],
-    },
-  },
-  
-  { 
-    intro n,
-    simp [my_add, ih],
-    induction n with n ih',
-    
-    {
-      simp [my_add],
-    },
-    
-    {
-      simp [my_add],
-      exact ih', 
-    },
-    
-  },
-  
-end
+theorem my_add_comm : ∀ (m n : my_nat), my_add m n = my_add n m := by
+  intro m n
+  revert m
+  induction n
+  -- we do the above so we can perform induction on the second arugment, while keeping the first argument quantified
+  case zero =>
+    intro m
+    induction m
+    case zero =>
+      simp
+    case succ m' ih_m =>
+      -- using left arrow inside of simp tactic, just tries to apply right hand side of the equality first
+      simp [my_add, ←ih_m]
+  case succ n' ih_n =>
+    intro m
+    induction m
+    case zero =>
+      have t := ih_n my_nat.zero
+      simp [my_add] at t
+      simp [my_add]
+      exact t
+    case succ m' ih_m =>
+      simp [my_add]
+      have t := ih_n (my_nat.succ m')
+      simp [← ih_m, t, my_add]
+      have t' := ih_n m'
+      simp [← t']
